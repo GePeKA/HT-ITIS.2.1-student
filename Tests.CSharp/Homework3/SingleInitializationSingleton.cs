94c62b164b6b45519d746ac67c48b90b235812e7
Maxim <111576269+GePeKA@users.noreply.github.com>
2023-09-26 21:21:36 +0300

    Completed HW3
    * settings for hw3
    
    * Created thread-safe SingleInitializationSingleton
    
    * Поправил тесты, которые не проходят в github actions
    
    Hw.3/WithMutex то проходит, то нет.
    EightThreads_100KIterations_RaceIsReproduced не может выполниться из-за высокой нагрузки
    
    * Исправил решение
    
    Сделал все через Lazy. Также подкорректировал тест, так как инициализация уже была проведена и нужно добавить Reset() в самом начале.
    
    * Добавил lock в некоторых местах

--- Tests.CSharp/Homework3/SingleInitializationSingleton.cs
+++ Tests.CSharp/Homework3/SingleInitializationSingleton.cs
@@ -3,9 +3,12 @@ namespace Tests.CSharp.Homework3;
 public class SingleInitializationSingleton
 {
     public const int DefaultDelay = 3_000;
+    
     private static readonly object Locker = new();
 
     private static volatile bool _isInitialized = false;
+
+    private static Lazy<SingleInitializationSingleton> _instance = new(() => new SingleInitializationSingleton());
 
     private SingleInitializationSingleton(int delay = DefaultDelay)
     {
@@ -16,15 +19,42 @@ public class SingleInitializationSingleton
 
     public int Delay { get; }
 
-    public static SingleInitializationSingleton Instance => throw new NotImplementedException();
+    public static SingleInitializationSingleton Instance
+    {
+        get
+        {
+            lock (Locker)
+            {
+                _isInitialized = true;
+                return _instance.Value;
+            }
+        }
+    }
 
     internal static void Reset()
     {
-        throw new NotImplementedException();
+        lock (Locker)
+        {
+            _isInitialized = false;
+            _instance = new Lazy<SingleInitializationSingleton>(() => new SingleInitializationSingleton());
+        }
     }
 
     public static void Initialize(int delay)
     {
-        throw new NotImplementedException();
+        if (!_isInitialized)
+        {
+            lock (Locker)
+            {
+                if (_isInitialized)
+                    throw new InvalidOperationException("Parameters were initialized in another thread");
+
+                _instance = new Lazy<SingleInitializationSingleton>(() => new SingleInitializationSingleton(delay));
+                _isInitialized = true;
+            }
+        }
+        
+        else
+            throw new InvalidOperationException("Parameters have already been initialized");
     }
 }
