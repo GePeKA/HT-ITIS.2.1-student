e3c00df7c84917098bb5d1dfd398304e6d0b2a2a
Maxim Lozhkin <maximlozhking2004@gmail.com>
2023-09-26 16:45:49 +0300

    Created thread-safe SingleInitializationSingleton

--- Tests.CSharp/Homework3/SingleInitializationSingleton.cs
+++ Tests.CSharp/Homework3/SingleInitializationSingleton.cs
@@ -3,9 +3,12 @@ namespace Tests.CSharp.Homework3;
 public class SingleInitializationSingleton
 {
     public const int DefaultDelay = 3_000;
+    
     private static readonly object Locker = new();
 
     private static volatile bool _isInitialized = false;
+
+    private static SingleInitializationSingleton? _instance = null;
 
     private SingleInitializationSingleton(int delay = DefaultDelay)
     {
@@ -16,15 +19,45 @@ public class SingleInitializationSingleton
 
     public int Delay { get; }
 
-    public static SingleInitializationSingleton Instance => throw new NotImplementedException();
+    public static SingleInitializationSingleton Instance
+    {
+        get
+        {
+            lock (Locker)
+            {
+                if (!_isInitialized)
+                {
+                    _instance = new SingleInitializationSingleton();
+                    _isInitialized = true;
+                }
+                return _instance!;
+            }
+        }
+    }
 
     internal static void Reset()
     {
-        throw new NotImplementedException();
+        lock (Locker)
+        {
+            _isInitialized = false;
+            _instance = null;
+        }
     }
 
     public static void Initialize(int delay)
     {
-        throw new NotImplementedException();
+        if (!_isInitialized)
+        {
+            lock (Locker)
+            {
+                if (_isInitialized)
+                    throw new InvalidOperationException("Instance has already been initialized in another thread");
+                
+                _isInitialized = true;
+                _instance = new SingleInitializationSingleton(delay);
+            }
+        }
+        else
+            throw new InvalidOperationException("Double initialization is prohibited");
     }
 }
