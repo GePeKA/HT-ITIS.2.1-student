903be3c19131c0d920363592c8e2a4f0a11a00fc
Maxim Lozhkin <maximlozhking2004@gmail.com>
2023-10-18 22:41:35 +0300

    Добавил консольный f# клиент
    Значения принимаются из консоли, затем асинхронно отправляется GET-запрос на сервер. Ответ сервера выводится в консоли

---
+++ Homework6/ConsoleWebCalculatorClient/Program.fs
@@ -0,0 +1,23 @@
+﻿open System 
+open System.Net.Http
+
+printf "%s" "Enter the first value: "
+let value1 = Console.ReadLine()
+
+Console.Write("Enter the second value: ")
+let value2 = Console.ReadLine()
+
+Console.Write("Enter the operation (Plus/Minus/Multiply/Divide): ")
+let operation = Console.ReadLine();
+
+let calculateOnServer() = 
+    async {
+        let url = $@"https://localhost:59811/calculate?value1={value1}&operation={operation}&value2={value2}"
+        use client = new HttpClient()
+        let! response = client.GetAsync url |> Async.AwaitTask
+        return! response.Content.ReadAsStringAsync() |> Async.AwaitTask
+    }
+
+let answer = calculateOnServer() |> Async.RunSynchronously
+
+printfn "%s" $"\nThe answer = {answer}"
