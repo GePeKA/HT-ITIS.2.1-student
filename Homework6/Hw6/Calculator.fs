6c48b48e4c272b7a1b6e2932783efac5eaba9c19
Maxim <111576269+GePeKA@users.noreply.github.com>
2023-10-19 20:01:08 +0300

    Ложкин 1.6
    * Settings
    
    * Реализовал калькулятор
    
    * Поменял CultureInfo
    
    * Добавил консольный f# клиент
    
    Значения принимаются из консоли, затем асинхронно отправляется GET-запрос на сервер. Ответ сервера выводится в консоли
    
    * Вынес калькулятор и парсер в отдельные сущности.
    
    Теперь calculatorHandler работает с контекстом, вызывает парсер и затем вызывает калькулятор
    
    * Подправил тест на DivideByZero
    
    Запрос с делением на ноль логически сделать плохим
    
    * Запрос деления на ноль все же OK
    
    Поменял на OK
    
    * Убрал ExcludeFromCodeCoverage
    
    * Убрал inline для того, чтобы не сбивалось покрытие кода
    
    * Переделал калькулятор
    
    Теперь Calculator.calculate возвращает Result<decimal, string>
    
    * Добавил тесты на калькулятор

---
+++ Homework6/Hw6/Calculator.fs
@@ -0,0 +1,21 @@
+﻿module Hw6.Calculator
+
+[<Literal>] 
+let Plus = "Plus"
+
+[<Literal>] 
+let Minus = "Minus"
+
+[<Literal>] 
+let Multiply = "Multiply"
+
+[<Literal>] 
+let Divide = "Divide"
+
+let calculate (value1:decimal) (operation:string) (value2:decimal) =
+    match operation with
+    | Plus -> Ok (value1 + value2)
+    | Minus -> Ok (value1 - value2)
+    | Multiply -> Ok (value1 * value2)
+    | Divide -> Ok (value1 / value2)
+    | _ -> Error $"Wrong Operation: {operation}"
--- Homework6/Hw6/Program.fs
+++ Homework6/Hw6/Program.fs
@@ -1,25 +1,38 @@
 module Hw6.App
 
-open System
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
+open Hw6.Parser
+open Hw6.Calculator
+
 
 let calculatorHandler: HttpHandler =
     fun next ctx ->
-        let result: Result<string, string> = raise(NotImplementedException())
+        let query1 = ctx.Request.Query["value1"] |> string
+        let query2 = ctx.Request.Query["value2"] |> string
+        let oper = ctx.Request.Query["operation"] |> string
+        
+        let result: Result<string, string> = 
+            match (parseCalcArguments query1 oper query2) with
+            | Ok(val1, op, val2) -> match (calculate val1 op val2) with
+                                    | Ok result -> Ok (result |> string)
+                                    | Error error -> Error error
+            | Error DivideByZero -> Ok (DivideByZero)
+            | Error error -> Error error
 
         match result with
-        | Ok ok -> (setStatusCode 200 >=> text (ok.ToString())) next ctx
-        | Error error -> (setStatusCode 400 >=> text error) next ctx
+        | Ok ok -> (setStatusCode 200 >=> text (ok)) next ctx
+        | Error error -> (setStatusCode 400 >=> text ($"Error occured: {error}")) next ctx
 
 let webApp =
     choose [
         GET >=> choose [
-             route "/" >=> text "Use //calculate?value1=<VAL1>&operation=<OPERATION>&value2=<VAL2>"
+             route "/" >=> text "Use /calculate?value1=<VAL1>&operation=<OPERATION>&value2=<VAL2>"
+             route "/calculate" >=> calculatorHandler
         ]
         setStatusCode 404 >=> text "Not Found" 
     ]
