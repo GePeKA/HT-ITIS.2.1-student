7632c591279a8a880e1609c7995229b3792a2e69
Maxim <111576269+GePeKA@users.noreply.github.com>
2023-11-09 13:23:45 +0300

    Ложкин 2.1.8
    * Settings
    
    * Added Dependency Injection
    
    * Поменял на конкретный класс
    
    * Поменял generic тип ActionResult
    
    * Added Calculator and CalculatorHandler

--- Homework8/Hw8/Controllers/CalculatorController.cs
+++ Homework8/Hw8/Controllers/CalculatorController.cs
@@ -7,12 +7,14 @@ namespace Hw8.Controllers;
 
 public class CalculatorController : Controller
 {
-    public ActionResult<double> Calculate([FromServices] ICalculator calculator,
+    public ActionResult<string> Calculate([FromServices] ICalculator calculator,
         string val1,
         string operation,
         string val2)
     {
-        throw new NotImplementedException();
+        var calculatorHandler = new CalculatorHandler(calculator);
+
+        return calculatorHandler.Solve(val1, operation, val2);
     }
     
     [ExcludeFromCodeCoverage]
