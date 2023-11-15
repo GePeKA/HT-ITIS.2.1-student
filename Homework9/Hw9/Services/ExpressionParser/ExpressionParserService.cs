1b52e4be7d6934faa8e820cfca930f7894fb041d
Maxim Lozhkin <maximlozhking2004@gmail.com>
2023-11-15 15:19:14 +0300

    Created ExpressionParser
    It parses the string by using polish notation and postfix calculator alghoritms

---
+++ Homework9/Hw9/Services/ExpressionParser/ExpressionParserService.cs
@@ -0,0 +1,251 @@
+﻿using System.Linq.Expressions;
+using static Hw9.ErrorMessages.MathErrorMessager;
+
+namespace Hw9.Services.ExpressionParser
+{
+    public class ExpressionParserService : IExpressionParserService
+    {
+        public const string Plus = "+";
+        public const string Minus = "-";
+        public const string Multiply = "*";
+        public const string Divide = "/";
+        public const string OpenBracket = "(";
+        public const string CloseBracket = ")";
+
+        public bool CheckExpressionString(string? expression, out string errorMessage)
+        {
+            if (expression == null)
+            {
+                errorMessage = EmptyString;
+                return false;
+            }
+
+            var openBracketsStack = new Stack<string>();
+
+            var symbols = expression.Split(' ');
+
+            var operators = new List<string>() { Plus, Minus, Multiply, Divide };
+
+            var previousSymb = string.Empty;
+
+            foreach (var symb in symbols)
+            {
+                double num;
+
+                if (double.TryParse(symb, out num))
+                {
+                    if (previousSymb == Divide && num == 0)
+                    {
+                        errorMessage = DivisionByZero;
+                        return false;
+                    }
+                }
+
+                else if (symb == OpenBracket)
+                {
+                    openBracketsStack.Push(symb);
+                }
+
+                else if (symb == CloseBracket)
+                {
+                    if (operators.Contains(previousSymb))
+                    {
+                        errorMessage = OperationBeforeParenthesisMessage(previousSymb);
+                        return false;
+                    }
+
+                    if (!openBracketsStack.TryPop(out _))
+                    {
+                        errorMessage = IncorrectBracketsNumber;
+                        return false;
+                    }
+                }
+
+                else if (operators.Contains(symb))
+                {
+                    if (operators.Contains(previousSymb))
+                    {
+                        errorMessage = TwoOperationInRowMessage(previousSymb, symb);
+                        return false;
+                    }
+
+                    if (previousSymb == string.Empty)
+                    {
+                        errorMessage = StartingWithOperation;
+                        return false;
+                    }
+
+                    if (previousSymb == OpenBracket && symb != Minus)
+                    {
+                        errorMessage = InvalidOperatorAfterParenthesisMessage(symb);
+                        return false;
+                    }
+                }
+
+                else
+                {
+                    if (char.TryParse(symb, out var chr))
+                    {
+                        errorMessage = UnknownCharacterMessage(chr);
+                        return false;
+                    }
+                    
+                    errorMessage = NotNumberMessage(symb);
+
+                    return false;
+                }
+
+                previousSymb = symb;
+            }
+
+            if (openBracketsStack.Count > 0)
+            {
+                errorMessage = IncorrectBracketsNumber;
+                return false;
+            }
+
+            if (operators.Contains(previousSymb))
+            {
+                errorMessage = EndingWithOperation;
+                return false;
+            }
+
+            errorMessage = string.Empty;
+            return true;
+        }
+
+        public string? FormatIntoCorrectExpressionString(string? expression)
+        {
+            return expression?.Replace("(", " ( ").Replace(")", " ) ").
+                Replace("/", " / ").Replace("+", " + ").Replace("-", " - ").Replace("*", " * ").
+                Replace("   ", " ").Replace("  ", " ").Trim();
+        }
+
+        public Expression MakeExpression(string expression)
+        {
+            var polish = MakePolishNotation(expression);
+            var expr = PostfixCalculate(polish);
+
+            return expr;
+        }
+
+        //Using Shunting-yard algorithm
+        private Queue<string> MakePolishNotation(string expression)
+        {
+            var resultQueue = new Queue<string>();
+
+            var operators = new List<string>() { Plus, Minus, Multiply, Divide, OpenBracket };
+
+            var tokens = expression.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+            var operatorStack = new Stack<string>();
+
+            foreach(var token in tokens) 
+            { 
+                if (operators.Contains(token))
+                {
+                    while (operatorStack.Count > 0 && HasHigherPriority(operatorStack.Peek(), token))
+                    {
+                        resultQueue.Enqueue(operatorStack.Pop());
+                    }
+
+                    operatorStack.Push(token);
+                }
+
+                else if (token == CloseBracket)
+                {
+                    var lastOper = operatorStack.Pop();
+
+                    while (lastOper != OpenBracket)
+                    {
+                        resultQueue.Enqueue(lastOper);
+                        lastOper = operatorStack.Pop();
+                    }
+                }
+
+                else if (double.TryParse(token, out _))
+                {
+                    resultQueue.Enqueue(token);
+                }
+            }
+
+            while (operatorStack.Count > 0)
+            {
+                resultQueue.Enqueue(operatorStack.Pop());
+            }
+
+            return resultQueue;
+
+            bool HasHigherPriority(string op1, string op2) => 
+                new List<string>() { Multiply, Divide }.Contains(op1)
+                    && new List<string>() { Plus, Minus }.Contains(op2);
+        }
+
+        //Postfix Calculator algorithm
+        private Expression PostfixCalculate(Queue<string> postfixQueue)
+        {
+            var exprStack = new Stack<Expression>();
+
+            while (postfixQueue.Count > 0)
+            {
+                var token = postfixQueue.Dequeue();
+
+                if (double.TryParse(token, out var number))
+                {
+                    exprStack.Push(Expression.Constant(number, typeof(double)));
+                }
+
+                else
+                {
+                    Expression expression;
+                    switch (token)
+                    {
+                        case (Plus):
+                            expression = Expression.Add(exprStack.Pop(), exprStack.Pop());
+                            break;
+
+                        case (Minus):
+                            var subtract = exprStack.Pop();
+                            var subtracted = exprStack.Pop();
+                            expression = Expression.Subtract(subtracted, subtract);
+                            break;
+
+                        case (Multiply):
+                            expression = Expression.Multiply(exprStack.Pop(), exprStack.Pop());
+                            break;
+
+                        case (Divide):
+                            var rightDivide = exprStack.Pop();
+                            var leftDivide = exprStack.Pop();
+
+                            if (rightDivide is ConstantExpression &&
+                                (double)(rightDivide as ConstantExpression)!.Value! == 0)
+                            {
+                                return Expression.Throw(Expression.Constant
+                                    (new DivideByZeroException(DivisionByZero)));
+                            }
+
+                            //If right part is BinaryExpression then we can check whether it's = 0 only by compiling and running it
+                            else if (rightDivide is BinaryExpression)
+                            {
+                                var expr = Expression.Lambda<Func<double>>((BinaryExpression)rightDivide);
+                                if (expr.Compile()() == 0)
+                                    return Expression.Throw(Expression.Constant
+                                        (new DivideByZeroException(DivisionByZero)));
+                            }
+
+                            expression = Expression.Divide(leftDivide, rightDivide);
+                            break;
+
+                        default:
+                            throw new Exception("Unexisting operator");
+                    };
+
+                    exprStack.Push(expression);
+                }
+            }
+
+            return exprStack.Pop();
+        }
+    }
+}
