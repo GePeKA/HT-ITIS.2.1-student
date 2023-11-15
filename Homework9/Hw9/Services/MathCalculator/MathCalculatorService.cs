01be4d1422a0d939a32208730809897167c3b7fa
Maxim Lozhkin <maximlozhking2004@gmail.com>
2023-11-15 15:19:37 +0300

    Implemented MathCalculator

--- Homework9/Hw9/Services/MathCalculator/MathCalculatorService.cs
+++ Homework9/Hw9/Services/MathCalculator/MathCalculatorService.cs
@@ -1,11 +1,39 @@
 using Hw9.Dto;
+using Hw9.Services.ExpressionParser;
+using System.Linq.Expressions;
 
 namespace Hw9.Services.MathCalculator;
 
 public class MathCalculatorService : IMathCalculatorService
 {
+    public IExpressionParserService ExpressionParser { get; set; }
+
+    public MathCalculatorService(IExpressionParserService expressionParserService)
+    {
+        ExpressionParser = expressionParserService;
+    }
+
     public async Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression)
     {
-        throw new NotImplementedException();
+        expression = ExpressionParser.FormatIntoCorrectExpressionString(expression);
+
+        if (!ExpressionParser.CheckExpressionString(expression, out var errorMessage))
+        {
+            return new CalculationMathExpressionResultDto(errorMessage);
+        }
+
+        var expr = ExpressionParser.MakeExpression(expression!);
+
+        if (expr.NodeType == ExpressionType.Throw)
+        {
+            var exception = (((expr as UnaryExpression)!.Operand as ConstantExpression)!
+                .Value as Exception)!.Message;
+
+            return new CalculationMathExpressionResultDto(exception);
+        }
+
+        var calculateExpression = await Task.Run(() => Expression.Lambda<Func<double>>(expr).Compile());
+
+        return new CalculationMathExpressionResultDto(calculateExpression());
     }
 }
