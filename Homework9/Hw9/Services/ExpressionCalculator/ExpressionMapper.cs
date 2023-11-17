850665e8f13a4460552792ec7809dceb553c20c0
Maxim Lozhkin <maximlozhking2004@gmail.com>
2023-11-17 11:12:52 +0300

    Added ExpressionMapper

---
+++ Homework9/Hw9/Services/ExpressionCalculator/ExpressionMapper.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq.Expressions;
+
+namespace Hw9.Services.ExpressionCalculator
+{
+    public class ExpressionMapper: ExpressionVisitor
+    {
+        private readonly Dictionary<Expression, List<Expression>> _expressionsMap = new();
+
+        public Dictionary<Expression, List<Expression>> MapExpression(Expression expression)
+        {
+            Visit(expression);
+            return _expressionsMap;
+        }
+
+        protected override Expression VisitConstant(ConstantExpression node)
+        {
+            _expressionsMap.Add(node, new List<Expression>());
+
+            return node;
+        }
+
+        protected override Expression VisitBinary(BinaryExpression binary)
+        {
+            _expressionsMap.Add(binary, new List<Expression>() { Visit(binary.Left), Visit(binary.Right) });
+
+            return binary;
+        }
+    }
+}
