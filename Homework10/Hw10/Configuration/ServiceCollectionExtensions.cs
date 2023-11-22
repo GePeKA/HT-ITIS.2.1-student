ea53a216c7446fc9491306054b1ac65d08661856
Maxim Lozhkin <maximlozhking2004@gmail.com>
2023-11-22 22:25:46 +0300

    Created proper MathCalculator based on Expressions

--- Homework10/Hw10/Configuration/ServiceCollectionExtensions.cs
+++ Homework10/Hw10/Configuration/ServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
 using Hw10.DbModels;
 using Hw10.Services;
 using Hw10.Services.CachedCalculator;
+using Hw10.Services.ExpressionParser;
+using Hw10.Services.ExpressionCalculator;
 using Hw10.Services.MathCalculator;
 
 namespace Hw10.Configuration;
@@ -18,5 +20,17 @@ public static class ServiceCollectionExtensions
             new MathCachedCalculatorService(
                 s.GetRequiredService<ApplicationContext>(), 
                 s.GetRequiredService<MathCalculatorService>()));
+    }
+
+    public static IServiceCollection AddExpressionParser(this IServiceCollection services)
+    {
+        return services
+            .AddTransient<IExpressionParserService, ExpressionParserService>();
+    }
+
+    public static IServiceCollection AddExpressionCalculator(this IServiceCollection services)
+    {
+        return services
+            .AddTransient<IExpressionCalculatorService, ExpressionCalculatorService>();
     }
 }
--- Homework10/Hw10/Services/MathCalculator/MathCalculatorService.cs
+++ Homework10/Hw10/Services/MathCalculator/MathCalculatorService.cs
@@ -1,11 +1,46 @@
 using Hw10.Dto;
+using System.Linq.Expressions;
+using Hw10.Services.ExpressionParser;
+using Hw10.Services.ExpressionCalculator;
+using static Hw10.ErrorMessages.MathErrorMessager;
 
 namespace Hw10.Services.MathCalculator;
 
 public class MathCalculatorService : IMathCalculatorService
 {
+    public IExpressionParserService ExpressionParser { get; set; }
+    public IExpressionCalculatorService ExpressionCalculator { get; set; }
+
+    public MathCalculatorService(IExpressionParserService expressionParserService,
+        IExpressionCalculatorService expressionCalculator)
+    {
+        ExpressionParser = expressionParserService;
+        ExpressionCalculator = expressionCalculator;
+    }
+
     public async Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression)
     {
-        throw new NotImplementedException();
+        Expression expr;
+
+        try
+        {
+            expr = ExpressionParser.ConstructExpression(expression!);
+        }
+        catch (Exception e)
+        {
+            return new CalculationMathExpressionResultDto(e.Message);
+        }
+
+        if (expr is ConstantExpression constant)
+        {
+            return new CalculationMathExpressionResultDto((double)constant.Value!);
+        }
+
+        var result = await ExpressionCalculator.CalculateExpressionAsync(expr);
+
+        if (double.IsFinite(result))
+            return new CalculationMathExpressionResultDto(result);
+        else
+            return new CalculationMathExpressionResultDto(DivisionByZero);
     }
 }
