0fd59c47a1dcabb79df2fb6ddd95d5cacb95b637
Maxim Lozhkin <maximlozhking2004@gmail.com>
2023-11-29 15:47:57 +0300

    Переделал MathCalculator
    Теперь нет обработки ошибок в try/catch, ведь ошибки теперь обрабатываются в контроллере

--- Homework11/Hw11/Services/MathCalculator/MathCalculatorService.cs
+++ Homework11/Hw11/Services/MathCalculator/MathCalculatorService.cs
@@ -1,9 +1,37 @@
+using Hw11.Dto;
+using Hw11.Services.ExpressionCalculator;
+using Hw11.Services.ExpressionParser;
+using System.Linq.Expressions;
+using static Hw11.ErrorMessages.MathErrorMessager;
+
 namespace Hw11.Services.MathCalculator;
 
 public class MathCalculatorService : IMathCalculatorService
 {
+    public IExpressionParserService ExpressionParser { get; set; }
+    public IExpressionCalculatorService ExpressionCalculator { get; set; }
+
+    public MathCalculatorService(IExpressionParserService expressionParserService,
+        IExpressionCalculatorService expressionCalculator)
+    {
+        ExpressionParser = expressionParserService;
+        ExpressionCalculator = expressionCalculator;
+    }
+
     public async Task<double> CalculateMathExpressionAsync(string? expression)
     {
-        throw new NotImplementedException();
+        Expression expr;
+
+        expr = ExpressionParser.ConstructExpression(expression!);
+
+        if (expr is ConstantExpression constant)
+            return (double)constant.Value!;
+
+        var result = await ExpressionCalculator.CalculateExpressionAsync(expr);
+
+        if (double.IsFinite(result))
+            return result;
+        else
+            throw new DivideByZeroException(DivisionByZero);
     }
 }
