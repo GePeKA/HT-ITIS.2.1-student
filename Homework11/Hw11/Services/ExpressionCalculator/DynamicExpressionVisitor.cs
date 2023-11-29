1872b05ed2bf76260169b482cdc8b101c63ea476
Maxim Lozhkin <maximlozhking2004@gmail.com>
2023-11-29 15:46:00 +0300

    Создал DynamicExpressionVisitor
    Вместо наследования ExpressionVisitor применил технику multiple dispatch

---
+++ Homework11/Hw11/Services/ExpressionCalculator/DynamicExpressionVisitor.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq.Expressions;
+
+namespace Hw11.Services.ExpressionCalculator
+{
+    public class DynamicExpressionVisitor
+    {
+        private readonly Dictionary<Expression, List<Expression>> _expressionsMap = new();
+
+        public Dictionary<Expression, List<Expression>> ConstructExecuteBeforeMap(Expression expression)
+        {
+            VisitExpression(expression);
+            return _expressionsMap;
+        }
+
+        public T VisitExpression<T>(T expression) where T: Expression
+        {
+            return Visit((dynamic)expression);
+        }
+
+        public Expression Visit(ConstantExpression constant) 
+        {
+            return constant;
+        }
+
+        public Expression Visit(BinaryExpression binary)
+        {
+            var left = VisitExpression(binary.Left);
+            var right = VisitExpression(binary.Right);
+
+            var executeBeforeList = new List<Expression>();
+
+            if (left is BinaryExpression leftBinary)
+                executeBeforeList.Add(leftBinary);
+
+            if (right is BinaryExpression rightBinary)
+                executeBeforeList.Add(rightBinary);
+
+            _expressionsMap.Add(binary, executeBeforeList);
+
+            return binary;
+        }
+    }
+}
