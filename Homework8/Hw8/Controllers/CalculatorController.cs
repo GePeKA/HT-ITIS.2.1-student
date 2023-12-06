5be297e6e7f16b21daca81f4368ef731b07fb6b5
Maxim Lozhkin <maximlozhking2004@gmail.com>
2023-12-06 12:05:19 +0300

    Добавил MiniProfiler

--- Homework8/Hw8/Controllers/CalculatorController.cs
+++ Homework8/Hw8/Controllers/CalculatorController.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using Hw8.Calculator;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,8 +19,6 @@ public class CalculatorController : Controller
     [ExcludeFromCodeCoverage]
     public IActionResult Index()
     {
-        return Content(
-            "Заполните val1, operation(plus, minus, multiply, divide) и val2 здесь '/calculator/calculate?val1= &operation= &val2= '\n" +
-            "и добавьте её в адресную строку.");
+        return View();
     }
 }
