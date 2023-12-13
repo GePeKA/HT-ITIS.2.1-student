bd10d9871e3654a3a1e6a44087f7d7c489a8bddf
Maxim Lozhkin <maximlozhking2004@gmail.com>
2023-12-13 21:15:44 +0300

    Уменьшил количество запросов для быстрой отработки тестов

--- Tests.CSharp/Homework13/MemoryTest.cs
+++ Tests.CSharp/Homework13/MemoryTest.cs
@@ -26,7 +26,7 @@ public class MemoryTest : IClassFixture<WebApplicationFactory<Hw13.Calculator.Pr
         var testDataGenerator = new TestDataGenerator();
         var list = testDataGenerator.GetValue();
         long size = 0;
-        for (var i = 0; i < 10000; i++)
+        for (var i = 0; i < 100; i++)
             foreach (var element in list)
             {
                 var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Calculator/CalculateMathExpression");
