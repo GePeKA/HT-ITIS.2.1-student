63aaeea60108bcb482cdce22a19891f6c42c676b
Maxim Lozhkin <maximlozhking2004@gmail.com>
2023-12-13 19:06:26 +0300

    Переделал кэширующий калькулятор

---
+++ Homework13/Hw13_Calculator/Services/CachedCalculator/MathCachedCalculatorService.cs
@@ -0,0 +1,27 @@
+﻿using Hw13.Calculator.Dto;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Hw13.Calculator.Services.CachedCalculator;
+
+public class MathCachedCalculatorService : IMathCalculatorService
+{
+	private readonly IMemoryCache _cache;
+	private readonly IMathCalculatorService _simpleCalculator;
+
+	public MathCachedCalculatorService(IMemoryCache cache, IMathCalculatorService simpleCalculator)
+	{
+		_cache = cache;
+		_simpleCalculator = simpleCalculator;
+	}
+
+	public async Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression)
+	{
+		return await _cache.GetOrCreateAsync(expression, async _ =>
+        {
+            var res = await _simpleCalculator.CalculateMathExpressionAsync(expression);
+            return res.IsSuccess
+                ? new CalculationMathExpressionResultDto(res.Result)
+                : new CalculationMathExpressionResultDto(res.ErrorMessage);
+        });
+    }
+}
